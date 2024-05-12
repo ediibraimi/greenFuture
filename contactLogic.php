bc371f3d6ff57077bba6285dd4788f2f8654b601
Albin161 <128827808+Albin161@users.noreply.github.com>
2024-05-12 10:09:35 +0200

    123

---
+++ contactLogic.php
@@ -0,0 +1,40 @@
+<?php 
+	session_start();
+
+	include_once('config.php');
+
+	if(isset($_POST['submit']))
+	{
+		$name = $_POST['name'];
+		$email = $_POST['email'];
+        $subject = $_POST['$subject'];
+        $messages = $_POST['$message'];
+		if (empty($name) || empty($email) || empty($subject)) {
+
+			echo "Please fill in all fields
+			";
+
+		}
+		else{
+			
+			$sql = "INSERT INTO contact(name,email,subject,messages) VALUES (:name, :email, :subject, :message)";
+
+			$insertSql = $conn->prepare($sql);
+			
+
+			$insertSql->bindParam(':name', $name);
+			$insertSql->bindParam(':email', $email);
+			$insertSql->bindParam(':subject', $subject);
+			$insertSql->bindParam(':message', $message);
+         
+			$insertSql->execute();
+
+			header("Location: index.php");
+
+		}
+
+
+	}
+
+
+ ?>
